a77af7c6096c07d8b410663b611ee8a739462ff5
Ori Shadmon <oshadmon@gmail.com>
2017-09-03 12:22:00 -0700

    reset "host" to default of 'localhost'

--- tests/test_db.py
+++ tests/test_db.py
@@ -10,7 +10,7 @@ from StreamingSQL.db import create_connection, execute_command
 from StreamingSQL.fonts import Colors, Formats
 
 """Default configuration to connect to the DB"""
-host = '10.0.0.122'
+host = 'localhost'
 port = 3306
 usr = 'root'
 paswd = ''
